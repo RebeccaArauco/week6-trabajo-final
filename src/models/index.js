378aebc6c08489bc5eaa2f9ee1c7c85a7f4e2ec2
Rebecca Arauco <becca.arauco@gmail.com>
2024-07-02 16:01:38 -0400

    eight commit

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
+const ProductImg = require("./ProductImg");
 const Purchase = require("./Purchase");
 const User = require("./User");
 
@@ -24,3 +25,6 @@ User.hasMany(Purchase)
 Purchase.belongsTo(Product)
 Product.hasMany(Purchase)
 
+//ProductImg - Product relation
+ProductImg.belongsTo(Product)
+Product.hasMany(ProductImg)
